3c3a763b0b49aa9fdf7fffa1b8fa4ee6d767ca88
Mariano Quattrocchi <marianoq_91@hotmail.com>
2022-01-14 19:53:18 -0300

    items

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import './App.css';
 /* import './bootstrap/dist/css/bootstrap.min.css'; */
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       {/* importo los componentes */}
+      
       <NavBar></NavBar>
+      <ItemListContainer 
+        lista1 = 'item1' 
+        lista2 = 'item2'   
+      ></ItemListContainer>
+
     </div>
   );
 }
