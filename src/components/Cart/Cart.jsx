8e1a5ae87462193e3011d937ce96bfb0c0a0c78f
Mariano Quattrocchi <marianoq_91@hotmail.com>
2022-02-21 16:22:11 -0300

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCartContext } from "../../context/CartContext";
 import Button from "react-bootstrap/esm/Button";
 import { Link } from "react-router-dom";
+import { addDoc, collection, documentId, getDocs, getFirestore, query, where, writeBatch } from "firebase/firestore";
 
 
 const Cart = () => {
@@ -9,8 +10,84 @@ const Cart = () => {
 
   const { cartList, vaciarCarrito, sumaTotal, borrarItem } = useCartContext()
 
-  return <div>
+  const [id, setId] = useState('')
+  const [dataForm, setDataForm] = useState({
+    email: '',
+    phone: '',
+    name: ''
+  })
 
+
+  //
+  const realizarCompra = async (e) => {
+    e.preventDefault()
+
+    // Nuevo objeto de orders    
+    let orden = {}
+
+    orden.buyer = dataForm //{name:'Federico',email: 'f@gmail.com', phone: '1234567890'}
+    orden.total = sumaTotal();
+
+    orden.items = cartList.map(cartItem => {
+      const id = cartItem.item.id;
+      const nombre = cartItem.item.name;
+      const precio = cartItem.item.price * cartItem.cantidad;
+      const cantidad = cartItem.cantidad
+
+      return {
+        id,
+        nombre,
+        precio,
+        cantidad
+      }
+    })
+
+    const db = getFirestore()
+    const ordersCollection = collection(db, 'orders')
+    await addDoc(ordersCollection, orden)
+      .then(resp => setId(resp.id))
+
+
+    const queryCollection = collection(db, 'items')
+
+
+    const queryActulizarStock = query(
+      queryCollection,
+      where(documentId(), 'in', cartList.map(it => it.item.id))
+    )
+
+    const batch = writeBatch(db)
+
+    await getDocs(queryActulizarStock)
+      .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
+        stock: res.data().stock - cartList.find(item => item.item.id === res.id).cantidad
+      })
+      ))
+      .catch(err => console.log(err))
+      .finally(() => {
+        setDataForm({
+          email: '',
+          phone: '',
+          name: ''
+        })
+        vaciarCarrito()
+      })
+    batch.commit()
+
+  }
+
+  const handleChange = (event) => {
+    setDataForm({
+      ...dataForm,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  console.log(dataForm);
+
+  return <div>
+    {id !== '' && `El id de la orden es : ${id} `}
+    <br />
     {cartList.length !== 0 ? <>
       {cartList.map(produ => <div>
         <li>{produ.product.name} precio: {produ.product.price} cantidad: {produ.cantidad}</li>
@@ -18,6 +95,44 @@ const Cart = () => {
       </div>)}
       {`la suma es ${sumaTotal()}`}
       <button onClick={vaciarCarrito} >Vaciar Carrito</button>
+      <br />
+
+      <form
+        onSubmit={realizarCompra}
+      >
+        <input
+          type='text'
+          name='name'
+          placeholder='name'
+          onChange={handleChange}
+          value={dataForm.name}
+        />
+        <br />
+        <input
+          type='number'
+          name='phone'
+          placeholder='tel'
+          onChange={handleChange}
+          value={dataForm.phone}
+        />
+        <br />
+        <input
+          type='email'
+          name='email'
+          placeholder='email'
+          onChange={handleChange}
+          value={dataForm.email}
+        />
+        <input
+          type='email'
+          name='validarEmail'
+          placeholder='Repetir Email'
+          onChange={handleChange}
+        //value={}
+        />
+        <br />
+        <button>Generar Orden</button>
+      </form>
     </>
       :
       <div>
@@ -26,9 +141,10 @@ const Cart = () => {
           <Button>Volver al home</Button>
         </Link>
       </div>
+
     }
-    
   </div>;
+
 };
 
 
